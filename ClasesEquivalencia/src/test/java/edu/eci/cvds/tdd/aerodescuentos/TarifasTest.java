5bfbb55692115b0cfd58d129818160f5235b7a00
migue1994 <migue24co@gmail.com>
2019-08-28 21:41:22 -0500

    Sin readme

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -5,127 +5,115 @@ import java.beans.Transient;
 import org.junit.*;
 
 public class TarifasTest{
-    /**
-     *
-     */
+
     
     private CalculadorDescuentos descuento = new CalculadorDescuentos();
     private double tarifa;
 
     @Test
-    public void diasMayorVeinteEdadMenordieciocho(){
-        try{
-            tarifa = descuento.calculoTarifa(10000,21,15);
+    public void diasMayorVeinteEdadMenordieciocho()
+    {
 
-            double resultado = 10000*0.8;
-    
-            Assert.assertEquals(resultado, tarifa,0);
-        }
-        catch(ExcepcionParametrosInvalidos e){
-            
-        }
+        tarifa = descuento.calculoTarifa(10000,21,15);
+
+        double resultado = 10000*0.8;
+
+        Assert.assertEquals(resultado, tarifa,0);
+
     }
 
 
     @Test 
     public void diasMayorVeinteEdadMayorSesentayCinco()
     {
-        try{
-            tarifa = descuento.calculoTarifa(10000,21,75);
-            double resultado = 10000*0.77;
-            Assert.assertEquals(resultado, tarifa,0);
-        }
-        catch(ExcepcionParametrosInvalidos e){
-        }
+
+        tarifa = descuento.calculoTarifa(10000,21,75);
+        double resultado = 10000*0.77;
+        Assert.assertEquals(resultado, tarifa,0);
+
     }
 
     @Test 
     public void diasMayorVeinteEdadEntreDieciochoySesentayCinco()
     {
-        try{
-            tarifa = descuento.calculoTarifa(10000,22,40);
+        tarifa = descuento.calculoTarifa(10000,22,40);
 
-            double resultado = 10000*0.85;
-    
-            Assert.assertEquals(resultado, tarifa,0);
-        }
-        catch(ExcepcionParametrosInvalidos e){
+        double resultado = 10000*0.85;
 
-        }
+        Assert.assertEquals(resultado, tarifa,0);
     }
 
     @Test 
     public void diasMenorVeinteEdadMenorDieciocho()
     {
-        try{
-            tarifa = descuento.calculoTarifa(10000,18,14);
+        tarifa = descuento.calculoTarifa(10000,18,14);
 
-            double resultado = 10000*0.95;
-    
-            Assert.assertEquals(resultado, tarifa,0);
-        }
-        catch(ExcepcionParametrosInvalidos e){
+        double resultado = 10000*0.95;
 
-        }
+        Assert.assertEquals(resultado, tarifa,0);
     }
     
     @Test 
     public void diasMenorVeinteEdadMayorSesentayCinco()
     {
-        try{
-            tarifa = descuento.calculoTarifa(10000,15,78);
 
-            double resultado = 10000*0.92;
-    
-            Assert.assertEquals(resultado, tarifa,0);
-        }
-        catch(ExcepcionParametrosInvalidos e){
+        tarifa = descuento.calculoTarifa(10000,15,78);
 
-        }
+        double resultado = 10000*0.92;
+
+        Assert.assertEquals(resultado, tarifa,0);
+
     }
     
     @Test 
     public void diasMenorVeinteEdadEntreDieciochoySesentayCinco()
     {
-        try{
-            tarifa = descuento.calculoTarifa(10000,14,40);
+        tarifa = descuento.calculoTarifa(10000,14,40);
 
-            double resultado = 10000;
-    
-            Assert.assertEquals(resultado, tarifa,0);
-        }
-        catch(ExcepcionParametrosInvalidos e){
-        }
+        double resultado = 10000;
+
+        Assert.assertEquals(resultado, tarifa,0);
     }
 
-    @Test 
-    public void edadInvalida()
+    @Test
+    public void diasIgualaVeinteEdadIgualDieciOcho()
     {
-        try{
-            tarifa = descuento.calculoTarifa(10000,21,-40);
+        tarifa = descuento.calculoTarifa(10000,20,18);
 
-            double resultado = 10000*0.85;
-    
-            Assert.assertEquals(resultado, tarifa,0);
-        }
-        catch(ExcepcionParametrosInvalidos e){
-            Assert.assertEquals(e.getMessage(), ExcepcionParametrosInvalidos.INVALID_AGE);
-        }
+        double resultado = 10000;
+
+        Assert.assertEquals(resultado, tarifa,0);
     }
+
+    @Test
+    public void diasIgualaVeinteEdadIgualSesentaYCinco()
+    {
+        tarifa = descuento.calculoTarifa(10000,20,65);
+
+        double resultado = 10000;
+
+        Assert.assertEquals(resultado, tarifa,0);
+    }
+
+    @Test
+    public void invalidAge(){
+        tarifa = descuento.calculoTarifa(10000,21,-40);
+
+        double resultado = 10000*0.8;
+
+        Assert.assertEquals(resultado, tarifa,0);
+    }
+    
 
     @Test 
     public void diasAntelacionInvalidos()
     {
-        try{
+       
             tarifa = descuento.calculoTarifa(10000,-21,40);
 
-            double resultado = 10000*0.85;
+            double resultado = 10000;
     
             Assert.assertEquals(resultado, tarifa,0);
-        }
-        catch(ExcepcionParametrosInvalidos e){
-            Assert.assertEquals(e.getMessage(), ExcepcionParametrosInvalidos.INVALID_DIASANTELACION);
-        }
+        
     }
-
 }
